90228366e7fe62f8c1de95ccc6afe09be49cc116
Left-Behind-Sic <dushaster@gmail.com>
2022-11-06 09:07:19 +0500

    works

--- pages/api/youtube.ts
+++ pages/api/youtube.ts
@@ -1,40 +1,39 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 const getString = (initialText: string, start: string, end: string) => {
-	const from = initialText.indexOf(start);
-	const to = initialText.indexOf(end);
+  const from = initialText.indexOf(start);
+  const to = initialText.indexOf(end);
 
-	return initialText.substring(from, to).replace(start, "");
+  return initialText.substring(from, to).replace(start, "");
 };
 
 const parseFirstVideo = async (name: string) => {
-	const data = await fetch(`https://www.youtube.com/c/${name}/videos`);
-	const result = await data.text();
+  const data = await fetch(`https://www.youtube.com/channel/${name}/videos`);
+  const result = await data.text();
 
-	const title = getString(
-		result,
-		'"title":{"runs":[{"text":"',
-		'"}],"accessibility":'
-	);
+  const title = getString(
+    result,
+    '"title":{"runs":[{"text":"',
+    '"}],"accessibility":'
+  );
+  const videoId = getString(
+    result,
+    '"gridVideoRenderer":{"videoId":"',
+    '","thumbnail":'
+  );
 
-	const videoId = getString(
-		result,
-		'"gridVideoRenderer":{"videoId":"',
-		'","thumbnail":'
-	);
+  const thumbnail = getString(result, '{"thumbnails":[{"url":"', '","width"');
 
-	const thumbnail = getString(result, '{"thumbnails":[{"url":"', '","width"');
-
-	return { title, videoId, thumbnail };
+  return { title, videoId, thumbnail };
 };
 
 export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-	const result = [
-		await parseFirstVideo("REDGroup"),
-		await parseFirstVideo("MaxShushval"),
-	];
-	res.status(200).json(result);
+  const result = [
+    await parseFirstVideo("UC1jD8_S4CLAxlVzIKf1AK4w"),
+    // await parseFirstVideo("UC1jD8_S4CLAxlVzIKf1AK4w"),
+  ];
+  res.status(200).json(result);
 }
